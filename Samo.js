a3b37ce6d8fcba9dd1e0fd2b3a95df979854dca0
benitogf <benitogf@outlook.com>
2019-01-29 07:20:27 +0800

    no class

--- Samo.js
+++ Samo.js
@@ -1,47 +1,40 @@
 // https://github.com/daviddoran/typescript-reconnecting-websocket/blob/master/reconnecting-websocket.ts
-import SamoBase from "./SamoBase"
-class Samo extends SamoBase {
+import { Base64 } from 'js-base64'
+import ky from 'ky'
+const Samo = {
     /**
-     * Setting this to true is the equivalent of setting all instances of Samo.debug to true.
+     * Setting this to true is the equivalent of setting all instances of this.debug to true.
      */
-    debugAll = false;
+    debugAll: false,
     // These can be altered by calling code
-    debug = false;
+    debug: false,
 
     // Time to wait before attempting reconnect (after close)
-    reconnectInterval = 1000;
+    reconnectInterval: 1000,
     // Time to wait for WebSocket to open (before aborting and retrying)
-    timeoutInterval = 2000;
+    timeoutInterval: 2000,
 
     // Should only be used to read WebSocket readyState
-    readyState;
+    readyState: null,
 
     // Whether WebSocket was forced to close by this client
-    forcedClose = false;
+    forcedClose: false,
     // Whether WebSocket opening timed out
-    timedOut = false;
+    timedOut: false,
 
     // List of WebSocket sub-protocols
-    protocols = [];
+    protocols: [],
 
     // The underlying WebSocket
-    ws;
-    url;
+    ws: null,
+    url: null,
 
     // Set up the default 'noop' event handlers
-    onopen = (ev) => { };
-    onclose = (ev) => { };
-    onconnecting = (ev) => { };
-    onmessage = (ev) => { };
-    onerror = (ev) => { };
-
-    constructor(address, protocols = []) {
-        super(address.split('/')[0]);
-        this.url = 'ws://' + address;
-        this.protocols = protocols;
-        this.readyState = WebSocket.CONNECTING;
-        this.connect(false);
-    }
+    onopen: (ev) => { },
+    onclose: (ev) => { },
+    onconnecting: (ev) => { },
+    onmessage: (ev) => { },
+    onerror: (ev) => { },
 
     connect(reconnectAttempt) {
         this.ws = new WebSocket(this.url, this.protocols);
@@ -95,7 +88,7 @@ class Samo extends SamoBase {
             this.log('Samo', 'onerror', this.url, event);
             this.onerror(event);
         };
-    }
+    },
 
     send(data) {
         if (this.ws) {
@@ -104,7 +97,7 @@ class Samo extends SamoBase {
         } else {
             throw new Error('INVALID_STATE_ERR : Pausing to reconnect websocket');
         }
-    }
+    },
 
     del(index) {
         if (this.ws) {
@@ -117,7 +110,7 @@ class Samo extends SamoBase {
         } else {
             throw new Error('INVALID_STATE_ERR : Pausing to reconnect websocket');
         }
-    }
+    },
 
     set(data, index) {
         if (this.ws) {
@@ -126,7 +119,7 @@ class Samo extends SamoBase {
         } else {
             throw new Error('INVALID_STATE_ERR : Pausing to reconnect websocket');
         }
-    }
+    },
 
     /**
      * Returns boolean, whether websocket was FORCEFULLY closed.
@@ -141,7 +134,7 @@ class Samo extends SamoBase {
             return true;
         }
         return false;
-    }
+    },
 
     /**
      * Additional public API method to refresh the connection if still open (close, re-open).
@@ -155,13 +148,79 @@ class Samo extends SamoBase {
             return true;
         }
         return false;
-    }
+    },
 
     log(...args) {
-        if (this.debug || Samo.debugAll) {
+        if (this.debug || this.debugAll) {
             console.debug.apply(console, args);
         }
-    }
-}
+    },
 
-export default Samo;
+    decode(evt) {
+        const msg = Base64.decode(JSON.parse(evt.data).data)
+        const data = msg !== '' ? JSON.parse(msg) : { created: 0, updated: 0, index: '', data: 'e30=' }
+        const mode = evt.currentTarget.url.replace('ws://' + this.address + '/', '').split('/')[0]
+        return this._decode(mode, data)
+    },
+
+    _decode(mode, data) {
+        return (mode === 'sa') ? Object.assign(data, { data: JSON.parse(Base64.decode(data['data'])) }) :
+            Array.isArray(data) ?
+                data.map((obj) => {
+                    obj['data'] = JSON.parse(Base64.decode(obj['data']))
+                    return obj
+                }) : []
+    },
+
+    encode(data, index) {
+        return JSON.stringify({
+            data: Base64.encode(JSON.stringify(data)),
+            index
+        })
+    },
+
+    async rstats(optionalAddress) {
+        const res = await ky.get(
+            'http://' + ((optionalAddress) ? optionalAddress : this.address)).json();
+
+        return res
+    },
+
+    async rget(mode, key, optionalAddress) {
+        const data = await ky.get(
+            'http://' + ((optionalAddress) ? optionalAddress : this.address) + '/r/' + mode + '/' + key).json();
+        return this._decode(mode, data)
+    },
+
+    async rpost(mode, key, data, optionalIndex, optionalAddress) {
+        const res = await ky.post(
+            'http://' + ((optionalAddress) ? optionalAddress : this.address) + '/r/' + mode + '/' + key,
+            {
+                json: {
+                    index: optionalIndex,
+                    data: Base64.encode(JSON.stringify(data))
+                }
+            }
+        ).json();
+
+        return res.index
+    },
+
+    async rdel(key, optionalAddress) {
+        return ky.delete(
+            'http://' +
+            ((optionalAddress) ? optionalAddress : this.address) +
+            '/r/' + key);
+    },
+
+    parseTime: (evt) => parseInt(JSON.parse(evt.data).data)
+}
+export default (address, protocols = []) => {
+    var e = Object.assign({}, Samo)
+    e.address = address.split('/')[0]
+    e.url = 'ws://' + address;
+    e.protocols = protocols;
+    e.readyState = WebSocket.CONNECTING;
+    e.connect(false);
+    return e
+}
